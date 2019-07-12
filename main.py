6a2e91f2b3c479f795cbbd51c5acbca060fd5e7a
WagnerJM <45420760+WagnerJM@users.noreply.github.com>
2019-07-12 12:39:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ QUEUE_NAME = "recorder"
 HOST = "localhost"
 
 def callback(ch, method, properties, body):
+    """
+    Callback function for consuming the queue
+    """
     print(f"[x] Task in the queue {body}")
+    # Creating instance of AudioRecorder
     recorder = AudioRecorder(body)
     recorder.create()
 
@@ -13,14 +17,20 @@ def callback(ch, method, properties, body):
 
 
 def __name__ == '__main__':
+    # Connecting to rabbitMQ Server
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host=HOST),
     )
+    # Creating channel
     channel = connection.channel()
+    # Listening on queue=QUEUE_NAME
     channel.queue.declare(queue=QUEUE_NAME)
 
+    # Callback when message is in the queue
     channel.basic_consume(
         queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True
     )
+
     print("[x] waiting for messages! To exit press CTRL-C")
+    # Start the server and listen to events
     channel.start_consuming()
--- recorder/recorder.py
+++ recorder/recorder.py
@@ -3,28 +3,63 @@ from selenium.webdriver.firefox.options import Options
 import sounddevice as sd
 from scipy.io.wavfile import write
 
+import logging
+
+# Create a custom logger
+name = __name__
+logger = logging.getLogger(name)
+
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler(f'/media/festplatte/public/logs/{name}.log')
+c_handler.setLevel(logging.INFO)
+f_handler.setLevel(logging.INFO)
+
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+
+# Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
+
 class AudioRecorder:
+
+    """
+    AudioRecorder:
+
+    Opens Spotify track with Selenium, plays the opend track and records the sound.
+    After recording saves to .wav file
+
+    """
 
     def __init__(self, track):
         self.prepare_browser(track['settings'])
 
     def prepare_browser(self, settings):
-        print("Browser options")
+        """
+        creates a selenium webbrowser session with a specific profile and headless option
+        returns created webdriver
+
+        """
+        logger.info("Browser options")
         options = Options()
-        print("Setting Browser headless")
+        logger.info("Setting Browser headless")
         options.headless = True
         profile = webdriver.FirefoxProfile(settings['profile_path'])
-        print("profile found")
+        logger.debug("profile found")
         driver = webdriver.Firefox(
             options=options, 
             firefox_profile=profile,
             executable_path=settings['driver_path']
             )
-        print("Browser created")
+        logger.info("Browser created")
         return driver
 
         def create(self, driver, track):
-            print("Creating track")
+            logger.info("Creating track")
             track_id = track['id']
             url = f"https://open.spotify.com/track/{track_id}"
             fs = track['settings']['framesize']
@@ -32,19 +67,25 @@ class AudioRecorder:
 
 
             driver.get(url)
-            print("URL started")
+            logger.info("URL started")
             
             driver.find_element_by_css_selector("button.btn.btn-green").click()
             
-            print("recording")
+            logger.info("recording")
             driver.find_element_by_css_selector("button.btn.btn-green").click()
             recording = sd.rec(int(fs * seconds), samplerate=fs, channels=2, blocking=True)
             driver.find_element_by_css_selector("button.btn.btn-green").click()
 
-            print("done")
+            logger.info("done")
             driver.quit()
-            print("saving file")
-            write(f"/media/festplatte/public/recordings/input/{track['trackname']}.wav", fs, recording)
-            print("File saved")
-            return True
+            logger.info("saving file")
+            
+            try:
+                write(f"/media/festplatte/public/recordings/input/{track['trackname']}.wav", fs, recording)
+                logger.info("File saved")
+                return True
+            except Exception as e:
+                logger.error("File could not be created")
+                logger.error(e)
+                
 
